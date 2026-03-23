9378fdbb93afe8f6d5d0014f774ac079842f53fc
Mario Carlos Chita <mariochiita19996@gmail.com>
2026-03-23 15:04:57 +0000

    Authentication

---
+++ src/main/java/dev/mario/auth/model/User.java
@@ -0,0 +1,19 @@
+package dev.mario.auth.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+}
---
+++ src/main/java/dev/mario/auth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.mario.auth.repository;
+
+import dev.mario.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
+}
