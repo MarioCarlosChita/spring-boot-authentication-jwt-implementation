8b3603faee3c7a6e6716c7765dea5eafa8b43ad9
Mario Carlos Chita <mariochiita19996@gmail.com>
2026-03-23 15:09:41 +0000

    Authentication

---
+++ src/main/java/dev/mario/auth/controller/AuthController.java
@@ -0,0 +1,52 @@
+package dev.mario.auth.controller;
+
+import dev.mario.auth.model.User;
+import dev.mario.auth.repository.UserRepository;
+import dev.mario.auth.security.JwtUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController  {
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder encoder;
+    @Autowired
+    JwtUtil jwtUtils;
+    @PostMapping("/signin")
+    public String authenticateUser(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.getUsername(),
+                        user.getPassword()
+                )
+        );
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return jwtUtils.generateToken(userDetails.getUsername());
+    }
+    @PostMapping("/signup")
+    public String registerUser(@RequestBody User user) {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            return "Error: Username is already taken!";
+        }
+        // Create new user's account
+        User newUser = new User(
+                null,
+                user.getUsername(),
+                encoder.encode(user.getPassword())
+        );
+        userRepository.save(newUser);
+        return "User registered successfully!";
+    }
+}
