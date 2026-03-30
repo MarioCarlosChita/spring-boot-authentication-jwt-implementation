6c3b8728b6baee304b586bee0b618685eafcdc96
Mario Carlos Chita <mariochiita19996@gmail.com>
2026-03-30 15:16:53 +0100

    Refactor and add refresh token.

--- src/main/java/dev/mario/auth/controller/AuthController.java
+++ src/main/java/dev/mario/auth/controller/AuthController.java
@@ -22,6 +22,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController  {
@@ -38,7 +41,7 @@ public class AuthController  {
     TokenBlackListService   tokenBlackListService;
 
     @PostMapping("/signin")
-    public String authenticateUser(@RequestBody User user) {
+    public ResponseEntity<?> authenticateUser(@RequestBody User user) {
         try {
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
@@ -47,7 +50,14 @@ public class AuthController  {
                     )
             );
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            return jwtUtils.generateToken(userDetails.getUsername());
+            String accessToken = jwtUtils.generateAccessToken(userDetails.getUsername());
+            String refreshToken = jwtUtils.generateRefreshToken(userDetails.getUsername());
+
+            Map<String,String> responseTokens=  new HashMap<>();
+            responseTokens.put("AccessToken", accessToken);
+            responseTokens.put("RefreshToken",refreshToken);
+
+            return ResponseEntity.ok(responseTokens);
         } catch (BadCredentialsException ex) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN ,"Invalid credentials");
         } catch (Exception ex) {
@@ -74,12 +84,13 @@ public class AuthController  {
     public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
         Authentication authentication =   SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication != null && authentication.isAuthenticated()) {
-            String token = jwtUtils.parseJwt(request);
-            tokenBlackListService.addToken(token);
-
-            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User is not authenticated");
         }
+        String token = jwtUtils.parseJwt(request);
+        tokenBlackListService.addToken(token);
+
+        new SecurityContextLogoutHandler().logout(request, response, authentication);
         return ResponseEntity.ok("Logout successfully");
     }
 }
--- src/main/java/dev/mario/auth/security/JwtUtil.java
+++ src/main/java/dev/mario/auth/security/JwtUtil.java
@@ -14,10 +14,16 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
+
     @Value("${jwt.secret}")
     private String jwtSecret;
+
     @Value("${jwt.expiration}")
     private int jwtExpirationMs;
+
+    @Value("${jwt.refresh_token_expiry}")
+    private int jwtRefreshTokenExpiry;
+
     private SecretKey key;
     // Initializes the key after the class is instantiated and the jwtSecret is injected,
     // preventing the repeated creation of the key and enhancing performance
@@ -26,11 +32,20 @@ public class JwtUtil {
         this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
     }
     // Generate JWT token
-    public String generateToken(String username) {
+    public String generateAccessToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtRefreshTokenExpiry))
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
